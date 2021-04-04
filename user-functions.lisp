f31c9dab370739863001c5a472d29e3b96643db9
slavon00 <ondra.slavik.ahoj@gmail.com>
2021-04-04 12:07:44 +0200

    user functions file v0.1-alpha

--- user-functions.lisp
+++ user-functions.lisp
@@ -1,13 +1,8 @@
-;USERE-FUNCTIONS
-;(num-to-tnum 42.123)
-;(tnum-to-num * 0.1)
-;(type-of *)
-
+;USER-FUNCTIONS
+;need to load tnums.lisp before
 ;funkce pro prevod tnumu na float
 (defun tnum-to-float (tnum eps)
   (float (tnum-to-num tnum eps)))
-
-;(tnum-to-float (num-to-tnum 42.123) 0.1)
 
 ;funkce pro ladeni -- vypisuje cisla o presnosti 1, 0.1, 0.01 ... 10^-n; pokud behem vypoctu dojde k chybe -- treba deleni nulou -- nevypise nic, vraci posledni vysledek jako racionalni cislo -- zlomek
 (defun tnum-print-first-n (tnum n)
@@ -18,61 +13,52 @@
         i #\tab
         (coerce result 'long-float))))))
 
-;(tnum-print-first-n (tnum-pi) 13)
-;(tnum-print-first-n (tnum-e) 14)
-
+;funkce pro inkrementaci tnumu o jednicku
 (defun tnum-1+ (tnum)
   (tnum+ (num-to-tnum 1) tnum))
 
+;funkce pro dekrementaci tnumu o jednicku
 (defun tnum-1- (tnum)
   (tnum- tnum (num-to-tnum 1)))
 
+;druha odmocnina
 (defun tnum-sqrt (tnum)
   (tnum-root (num-to-tnum 2) tnum))
 
+;zlaty rez
 (defun tnum-phi ()
   (tnum/ (tnum-1+ (tnum-sqrt (num-to-tnum 5))) (num-to-tnum 2)))
 
-;(tnum-to-float (tnum-phi) -5)
-
+;tnum1-ty logaritmus tnumu2
 (defun tnum-log (tnum1 tnum2)
   (tnum/ (tnum-ln tnum2) (tnum-ln tnum1)))
 
-;(tnum-print-first-n (tnum-sin (num-to-tnum 1)) 15)
-
+;sinus tnumu i cisla
 (defun tsin (arg)
   (when (realp arg)
     (setf arg (num-to-tnum arg)))
   (tnum-sin arg))
 
-;(tnum-to-float (tsin 1) -6)
-
+;scitani tnumu a cisel
 (defun t+ (&rest args)
-  (tnum+ (apply 'tnum+ (print (remove-if 'realp args)))
-    (num-to-tnum (apply '+ (print (remove-if-not 'realp args))))))
+  (tnum+ (apply 'tnum+ (remove-if 'realp args))
+    (num-to-tnum (apply '+  (remove-if-not 'realp args)))))
 
-;(tnum-to-float (t+ 1 (tnum-pi) 2 (tnum-e) 3) -6)
-
+;nasobeni tnumu a cisel
 (defun t* (&rest args)
   (tnum*num (apply 'tnum* (remove-if 'realp args))
     (apply '* (remove-if-not 'realp args))))
 
-;(tnum-print-first-n (t* (tnum-phi) 4.2) 10)
-
+;kalkulackovy sinus vracejici long-float sinu cisla
 (defun calc-sin (num)
   (coerce (tnum-to-num (tsin num) -20) 'long-float))
 
-;(calc-sin 2)
-
+;nastaveni presnosti kalkulacky
 (defvar *calc-eps* -6)
+;nastaveni konverze z kalkulacky
 (defvar *calc-conversion* (lambda (num) (float num)))
 
+;sinus z nastavitelne kalkulacky
 (defun global-sin (num)
   (funcall *calc-conversion* 
     (tnum-to-num (tsin num) *calc-eps*)))
-
-;(global-sin 3)
-;(setq *calc-eps* -20)
-;(setq *calc-conversion* (lambda (num)
-;    (coerce num 'long-float)))
-;(global-sin 4)
